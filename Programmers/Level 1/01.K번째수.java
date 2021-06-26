1d023bd2e001d7ff8c016c724f1ff39b483420ed
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-06-26 23:37:30 +0900

    Update 01.K번째수.java

--- Programmers/Level 1/01.K번째수.java
+++ Programmers/Level 1/01.K번째수.java
@@ -1,19 +1,31 @@
 // https://docs.oracle.com/javase/7/docs/api/java/util/Arrays.html
 
+/*
+java.lang.Object
+ - java.util.Arrays
+*/
+
 import java.util.Arrays;
+import java.lang.reflect.Method;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         int cnt = 0;
+        showClassMethods();
         for(int[] command : commands) {
             int[] copy_command = Arrays.copyOfRange(array, command[0] - 1, command[1]);
             Arrays.sort(copy_command); 
             answer[cnt++] = copy_command[command[2] - 1];
-            
-            System.out.println(Arrays.toString(copy_command));
-            System.out.println(Arrays.toString(answer) + "\n");
         }
         return answer;
     }
+    
+    public void showClassMethods() {
+        Class testClass = Arrays.class;
+        Method[] methods = testClass.getMethods();
+        for(Method method : methods) 
+            System.out.println(method);
+    }
 }
+
