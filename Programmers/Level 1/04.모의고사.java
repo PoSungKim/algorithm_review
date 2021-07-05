3731ff2d49e5ae1bd2e358782f869707cb46d503
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-07-06 08:19:56 +0900

    Create 04.모의고사.java

---
+++ Programmers/Level 1/04.모의고사.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    
+    ArrayList<ArrayList<Integer>> one = new ArrayList<ArrayList<Integer>>();
+     
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+        System.out.println(Arrays.toString(answers));
+        one.add(new ArrayList<Integer>(Arrays.asList(new Integer[]{1,2,3,4,5})));
+        System.out.println(one);
+        
+        
+        return answer;
+    }
+}
