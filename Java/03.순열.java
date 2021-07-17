7bb9cbaea3d3a5f89e279a894965332864083887
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-07-17 19:57:22 +0900

    Create 03.순열.java

---
+++ Java/03.순열.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+class Solution {
+    
+    int[] list = new int[] {1,2,3,4,5};
+    int[] ret  = new int[list.length];
+    
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+        
+        // nPr
+        int r = 3;
+        perm(0, r, 0);
+        
+        return 0;
+    }
+    
+    public void perm(int cnt, int r, int flag) {
+        if (cnt == r) {
+            System.out.println(Arrays.toString(ret));
+            return;
+        }
+        
+        for(int i = 0; i < list.length; i++) {
+            if ( (flag & (1 << i)) != 0) continue;
+            ret[cnt] = list[i];
+            perm(cnt + 1, r, flag | 1 << i);
+        }
+    }
+}
