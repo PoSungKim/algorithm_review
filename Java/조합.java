471435c0dc6529f92c7fb32c024c074f5c6d25cd
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-07-17 19:36:34 +0900

    Create 조합.java

---
+++ Java/조합.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+class Solution {
+    
+    int[] list = new int[] {1,2,3,4,5};
+    
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+        
+        // nC0... nCn
+        comb1();
+        // nCr
+        comb2(3);
+        
+        return 0;
+    }
+    
+    public void comb1() {
+        for(int i = 0; i < (1 << list.length); i++) {
+            for(int j = 0; j < list.length; j++) {
+                if ((i & (1 << j)) > 0 ) {
+                    System.out.print(list[j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+    public void comb2(int r) {
+        for(int i = 0; i < (1 << list.length); i++) {
+            for(int j = 0; j < list.length; j++) {
+                if (Integer.bitCount(i) == r && (i & (1 << j)) > 0 ) {
+                    System.out.print(list[j] + " ");
+                }
+            }
+            if (Integer.bitCount(i) == r) System.out.println();
+        }
+    }
+}
