87369e6ad924f87c380c3def4db871aea2deea65
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-07-20 08:13:35 +0900

    Update 12.문자열 압축.java

--- Programmers/Level 1/12.문자열 압축.java
+++ Programmers/Level 1/12.문자열 압축.java
@@ -4,7 +4,7 @@ class Solution {
     public int solution(String s) {
         int ret = 987654321;
 
-        // 이렇게 나눗셈이 들어간 부분에서는 exception case를 조심해야 한다 : "a"처럼 길이가 1인 문자열이 들어올 수 있다
+        // 1.1 이렇게 나눗셈이 들어간 부분에서는 exception case를 조심해야 한다 : "a"처럼 길이가 1인 문자열이 들어올 수 있다
         for(int i = s.length() / 2; i > 0; i--) {
             StringBuilder sb = new StringBuilder(s), ans = new StringBuilder();
 
@@ -19,8 +19,8 @@ class Solution {
 
             ans.append(sb);
             ret = Math.min(ret, ans.length());
-        }
-
+        } 
+        // 1.2 "a"처럼 길이가 1이면, 압축이 불가능해서 답이 1과 같다
         return Math.min(ret, s.length());
     }
 }
