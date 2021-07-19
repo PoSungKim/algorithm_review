ebbc9cc2b4d773b800215d55d2a5db2a845ef046
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-07-20 08:11:18 +0900

    Update 12.문자열 압축.java

--- Programmers/Level 1/12.문자열 압축.java
+++ Programmers/Level 1/12.문자열 압축.java
@@ -4,6 +4,7 @@ class Solution {
     public int solution(String s) {
         int ret = 987654321;
 
+        // 이렇게 나눗셈이 들어간 부분에서는 exception case를 조심해야 한다 : "a"처럼 길이가 1인 문자열이 들어올 수 있다
         for(int i = s.length() / 2; i > 0; i--) {
             StringBuilder sb = new StringBuilder(s), ans = new StringBuilder();
 
