09cf40357276cabe435f414caf9c82d6df5fe7bb
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-07-19 22:09:16 +0900

    Create 12.문자열 압축.java

---
+++ Programmers/Level 1/12.문자열 압축.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int ret = 987654321;
+
+        for(int i = s.length() / 2; i > 0; i--) {
+            StringBuilder sb = new StringBuilder(s), ans = new StringBuilder();
+
+            while(sb.length() >= i) {
+                int cnt = 0; String tmp = sb.substring(0, i);
+                while(sb.length() >= i && tmp.equals(sb.substring(0, i))) {
+                    cnt++;
+                    sb = new StringBuilder(sb.substring(i));
+                }
+                ans.append(cnt > 1 ? String.valueOf(cnt) + tmp : tmp);
+            }
+
+            ans.append(sb);
+            ret = Math.min(ret, ans.length());
+        }
+
+        return Math.min(ret, s.length());
+    }
+}
