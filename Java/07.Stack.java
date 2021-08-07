aa50865b0ba0abe7eef4722f3a6b0f04f14cb26d
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-08-07 19:12:52 +0900

    Update 07.Stack.java

--- Java/07.Stack.java
+++ Java/07.Stack.java
@@ -1,1 +1,13 @@
 
+Class 
+
+Stack<Character> Stack = new Stack<>();
+Stack.push('a'); // ['a']
+Stack.push('b'); // ['a']
+Stack.push('c');
+
+System.out.println(Stack.peek());
+
+Stack.pop();
+Stack.pop();
+Stack.pop();
