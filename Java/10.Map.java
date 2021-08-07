4a58be1eadd5c0006c50678a97302843149e9d33
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-08-07 20:05:55 +0900

    Create 10.Map.java

---
+++ Java/10.Map.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    List<Map.Entry<Integer, Integer>> List = new ArrayList<>();
+    Map<Integer, Integer> HashMap = new HashMap<>();
+    Map<Integer, Integer> TreeMap = new TreeMap<>();
+    Map<Integer, Integer> LinkedHashMap = new LinkedHashMap<>();
+
+    public int solution(int[] A, int[] B) {
+        // A : [4, 3, 2, 1, 5]
+        // B : [0, 1, 0, 0, 0]
+        for(int i = 0; i < A.length; i ++)  {
+            List.add(new AbstractMap.SimpleEntry<Integer, Integer>(A[i], B[i]));
+            HashMap.put(A[i], B[i]);
+            TreeMap.put(A[i], B[i]);
+            LinkedHashMap.put(A[i], B[i]);
+        }
+            
+        System.out.println(List);          // [4=0, 3=1, 2=0, 1=0, 5=0] 
+        System.out.println(HashMap);       // {1=0, 2=0, 3=1, 4=0, 5=0} >> Iteration Order is not guaranteed       >> using Hash Table
+        System.out.println(TreeMap);       // {1=0, 2=0, 3=1, 4=0, 5=0} >> Iteration Order is guaranteed           >> using Red-Black Tree
+        System.out.println(LinkedHashMap); // {4=0, 3=1, 2=0, 1=0, 5=0} >> Iteration Order follows Insertion Order >> using doubly-linked list buckets 
+        
+        for(Map.Entry<Integer, Integer> e : List ) // {List, HashMap.entrySet(), TreeMap.entrySet(), LinkedHashMap.entrySet()}  
+            System.out.print("{ " + e.getKey() + ", " + e.getValue() + " } ");
+
+        return 0;
+    }
+}
