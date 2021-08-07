9ad2517eb604dae19e4ed2b152192b29f88fccb7
PoSungKim <psungkr94@likelion.org>
2021-08-07 23:36:42 +0900

    add codility

---
+++ Codility/lesson/Stacks and Queues/Fish.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+
+    Stack<Integer> Stack = new Stack<>();
+    public int solution(int[] A, int[] B) {
+        int curPos = 0;
+        while(curPos < A.length){
+            boolean didWhile = false;
+            while (curPos < A.length && !Stack.isEmpty() && B[Stack.peek()] == 1 && B[curPos] == 0) {
+                didWhile = true;
+                if (A[Stack.peek()] > A[curPos]) {
+                    curPos++;
+                    break;
+                } else {
+                    Stack.pop();
+                }
+            }
+            if (didWhile == false)
+                Stack.push(curPos++);
+        }
+        return Stack.size();   
+    }
+}
