1159be8e87ecc9ede654eb0af5085f5973f2b5c0
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-08-08 13:50:57 +0900

    Update 08.Deque.java

--- Java/08.Deque.java
+++ Java/08.Deque.java
@@ -1,1 +1,24 @@
+class Solution {
+    Deque<Integer> Deq = new ArrayDeque<Integer>();
+  
+    public int[] solution(int[] A, int K) {
+        Deq.addFirst(1);     // [1]
+        Deq.addLast(2);      // [1, 2]
+        Deq.offerFirst(3);   // [3, 1, 2]
+        Deq.offerLast(4);    // [3, 1, 2, 4]
+      
+        System.out.println(Deq.peekFirst()); // 3
+        System.out.println(Deq.peekLast());  // 4
+        System.out.println(Deq);             // [3, 1, 2, 4]
+      
+        Deq.removeFirst(1);  // [1, 2, 4]
+        Deq.removeLast(1);   // [1, 2]
+        Deq.pollFirst(1);    // [2]
+        Deq.pollLast(1);     // []
+      
+        return A;
+    }
+}
 
+// add vs offer >> exception vs false
+// remove vs poll >> exception vs null
