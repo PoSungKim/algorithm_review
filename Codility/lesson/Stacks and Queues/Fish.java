0d8946174b1e1997d0c09042b6121ea0804496ba
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-08-08 11:30:07 +0900

    Update Fish.java

--- Codility/lesson/Stacks and Queues/Fish.java
+++ Codility/lesson/Stacks and Queues/Fish.java
@@ -7,10 +7,10 @@ class Solution {
 
         for(int curPos = 0; curPos < A.length;)
             if (!Stack.isEmpty() && B[Stack.peek()] == 1 && B[curPos] == 0)
-                if (A[Stack.peek()] > A[curPos]) 
+                if (A[Stack.peek()] > A[curPos]) // 내려오는 물고기가 더 커서 올라오는 물고기를 먹는 케이스
                     curPos++;
-                else 
-                    Stack.pop();
+                else                             // 올라오는 물고기가 더 커서 내려오는 물고기를 먹는 케이스
+                    Stack.pop(); 
             else 
                 Stack.push(curPos++);
                 
