44a55c578ac7c58e87ab846ffb0093ffd357f81f
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-08-08 14:06:43 +0900

    Update 08.Deque.java

--- Java/08.Deque.java
+++ Java/08.Deque.java
@@ -10,10 +10,20 @@ class Solution {
         System.out.println(Deq.peekFirst()); // 3
         System.out.println(Deq.peekLast());  // 4
         System.out.println(Deq);             // [3, 1, 2, 4]
+        
+        System.out.println(Deq.remove(2));   // true 
+        System.out.println(Deq);             // [3, 1, 4]
+        System.out.println(Deq.offerFirst(2)); // [2, 3, 1, 4]
+        
+        Iterator<Integer> iter = Deq.iterator(); // Deq.descendingIterator();
+        while (iter.hasNext()) {
+            System.out.print(iter.next() + " "); // 3 1 2 4 
+        }
+        iter.forEachRemaining(x -> System.out.print(x + " ")) // 3 1 2 4 >> hasNext()와 forEachRemaining() 둘 중에 하나만 사용
       
-        Deq.removeFirst(1);  // [1, 2, 4]
-        Deq.removeLast(1);   // [1, 2]
-        Deq.pollFirst(1);    // [2]
+        Deq.removeFirst(1);  // [3, 1, 4]
+        Deq.removeLast(1);   // [3, 1]
+        Deq.pollFirst(1);    // [1]
         Deq.pollLast(1);     // []
       
         return A;
