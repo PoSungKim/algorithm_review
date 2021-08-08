eedd2ca62e6602918529685a875b476915b86740
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-08-08 22:57:15 +0900

    Update 05-2.Priority Queue(Map.Entry<Integer, Integer>).java

--- Java/05-2.Priority Queue(Map.Entry<Integer, Integer>).java
+++ Java/05-2.Priority Queue(Map.Entry<Integer, Integer>).java
@@ -3,6 +3,16 @@ import java.util.*;
 class Solution {
 
     Map<Integer, Integer> Map = new TreeMap<>();
+    
+    class myCompare implements Comparator<Map.Entry<Integer, Integer>>{
+        @Override
+        public int compare(Map.Entry<Integer, Integer> a, Map.Entry<Integer, Integer> b) {
+                if (b.getKey() == a.getKey())
+                    return b.getValue() - a.getValue() ;
+                
+                return  b.getKey() - a.getKey() ;
+            }
+    };
 
     public int solution(int X, int Y, int D) {
         
@@ -19,8 +29,11 @@ class Solution {
                 return a.getKey() - b.getKey() ;
             }
         });
+        
+        PriorityQueue<Map.Entry<Integer, Integer>> PQ2 = new PriorityQueue<>(new myCompare());
       
         PQ.addAll(Map.entrySet());
+        PQ2.addAll(Map.entrySet());
         
         System.out.println(PQ);              // Shows elements in heap (not necessarily ordered) >> don't be tricked! 
         
