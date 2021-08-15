871012aee0a9b6559b6c7b0c63b32a3ff6ad7595
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-08-15 13:03:39 +0900

    Update Limit&offset.sql

--- SQL/MySQL/Limit&offset.sql
+++ SQL/MySQL/Limit&offset.sql
@@ -3,3 +3,22 @@ SELECT
 FROM
     table_name
 LIMIT [offset,] row_count;
+
+
+#############################
+
+SELECT select_list
+FROM table_name
+ORDER BY sort_expression
+LIMIT n-1, 1;
+
+#############################
+
+SELECT 
+    customerName, 
+    creditLimit
+FROM
+    customers
+ORDER BY 
+    creditLimit DESC    
+LIMIT 1,1;
