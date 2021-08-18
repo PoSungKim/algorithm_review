98294d36de3f5ea447ce0da8972236ff88731695
PoSungKim <psungkr94@likelion.org>
2021-08-19 00:54:42 +0900

    Git Push to Pskim

--- Java/18.Dijkstra.java
+++ Java/18.Dijkstra.java
@@ -8,13 +8,7 @@ public class Dijkstra  {
     public static int[][] Graph = new int[MAX_N][MAX_N]; //그래프의 모양 (Edge가 어떻게 연결되어 있는지)
     public static int[] Dist = new int[MAX_N];           //그래프의 노드 간의 거리 (<<시작 노드>>에서 다른 노드까지의 최소거리이기에 1차원 배열)    
 
-    public static void listDist() {
-        System.out.println("Listing the Distance");
-        for(int i = 0; i < N; i++)
-            System.out.print(Dist[i] + " ");
-    }
-
-    public static void findShortestPath(int startNode) {
+    public static void findShortestDist(int startNode) {
         PriorityQueue<int[]> PQ = new PriorityQueue<>((a,b)->a[0] - b[0]);
         boolean [] Visited = new boolean[MAX_N];
         for(int i = 0; i < N; i++) Dist[i] = INF;
@@ -41,6 +35,13 @@ public class Dijkstra  {
                 }
             }
         }
+    }
+
+    public static void listDist() {
+        System.out.print("\nListing the Distance : ");
+        for(int i = 0; i < N; i++)
+            System.out.print(Dist[i] + " ");
+        System.out.println();
     }
 
     public static void setUpGraph() {
@@ -77,9 +78,88 @@ public class Dijkstra  {
     }
 
     public static void main(String[] args ) {
+        System.out.println("=================================");
+        System.out.println("PQ로 Shortest Distance");
+        System.out.println("=================================");
         setUpGraph();
         showGraph();
-        findShortestPath(0);
+        findShortestDist(0);
         listDist();
+
+        System.out.println("=================================");
+        System.out.println("PQ로 Shortest Path");
+        System.out.println("=================================");
+        findShortestPath(0);
+        listFullPath();
+
+        System.out.println("=================================");
+        System.out.println("시작 노드에서 다른 노드 별 거리");
+        System.out.println("=================================");
+        for(int i = 1; i < N; i++)
+            System.out.print(findEachDist(0, i) + " ");
+        System.out.println();
+    }
+
+    public static void listFullPath() {
+        int index = 5; // 원하는 도착지점 계산하면 됌
+        while(index != -1) {
+            System.out.print(index + " < ");
+            index = Prev[index];
+        }
+        System.out.println();
+    }
+
+    public static int[] Prev = new int[MAX_N]; //현재의 노드 번호의 전 노드 번호 (번호 : index)를 담는 Array
+    public static void findShortestPath(int startNode) {
+        PriorityQueue<int[]> PQ = new PriorityQueue<>((a,b)->a[0] - b[0]);
+        boolean[] Visited = new boolean[MAX_N];
+        for(int i = 0; i < N; i++) Dist[i] = INF;
+        
+        Dist[startNode] = 0;
+        Prev[startNode] = -1;
+        PQ.offer(new int[]{0, startNode});
+
+        while(!PQ.isEmpty()) {
+            int[] info = PQ.poll();
+            int u = info[1];
+
+            if (Visited[u]) continue;
+            Visited[u] = true;
+
+            for(int v = 0; v < N; v++) {
+                if (Dist[v] > Dist[u] + Graph[u][v]) {
+                    Dist[v] = Dist[u] + Graph[u][v];
+                    Prev[v] = u; // v의 전 노드는 u
+                    PQ.offer(new int[]{Dist[v], v});
+                }
+            }
+        }
+    }
+
+    public static int findEachDist(int startNode, int endNode) {
+        PriorityQueue<int[]> PQ = new PriorityQueue<>((a, b)-> a[0] - b[0]);
+        for(int i = 0; i < N; i ++) Dist[i] = INF;
+        boolean[] Visited = new boolean[MAX_N];
+        
+        Dist[startNode] = 0;
+        PQ.offer(new int[]{0, startNode});
+        
+        while(!PQ.isEmpty()) {
+            int[] info = PQ.poll();
+            int u = info[1];
+
+            if (u == endNode) return info[0];
+            if (Visited[u]) continue;
+            Visited[u] = true;
+
+            for(int v = 0; v < N; v++) {
+                if (Dist[v] > Dist[u] + Graph[u][v]) {
+                    Dist[v] = Dist[u] + Graph[u][v];
+                    PQ.offer(new int[] {Dist[v], v});
+                }
+            }
+        }
+
+        return INF;
     }
 }
