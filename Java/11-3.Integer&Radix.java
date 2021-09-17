b832dfca7f26e0033699a3bd7ed1fb2708982962
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-09-17 22:30:13 +0900

    Update 11-3.Integer&Radix.java

--- Java/11-3.Integer&Radix.java
+++ Java/11-3.Integer&Radix.java
@@ -2,9 +2,9 @@ class Solution {
     // 문자열 reverse나 비교는 String 사용
     public int String() {
       // radix 10
-      System.out.println(Integer.toString (1234  ));     // 1234 as String
-      System.out.println(Integer.parseInt ("1234"));     // 1234 as int
-      System.out.println(Integer.valueOf  ("1234"));     // 1234 as Integer
+      System.out.println(Integer.toString (1234  ));     // 1234  as String
+      System.out.println(Integer.parseInt ("1234"));     // 1234  as int
+      System.out.println(Integer.valueOf  ("1234"));     // 1234  as Integer
       
       // radix 5
       // 10 >> 5
@@ -13,5 +13,7 @@ class Solution {
       System.out.println(Integer.parseInt ("1234", 5));  // 194   as int
       // 5 >> 10 
       System.out.println(Integer.valueOf  ("1234", 5));  // 194   as Integer   
+        
+      System.out.println(Integer.parseInt ("1234", 2));  // java.lang.NumberFormatException 
     }
 }
