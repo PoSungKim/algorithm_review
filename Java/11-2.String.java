e269b3356479df59dce34e2dd74983f61ec36692
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-09-17 22:12:03 +0900

    Update 11-2.String.java

--- Java/11-2.String.java
+++ Java/11-2.String.java
@@ -6,21 +6,21 @@ class Solution {
       // https://mine-it-record.tistory.com/133 
 	    
       // ==           : 참조하는 주소 값 비교
-	    // .equals()    : 참조하는 String 클래스의 값 비교
-	    // .compareTo() : 참조하는 String 클래스의 값 비교 (1) 길이 같다면, 다른 char의 차이값 리턴 (2) 길이가 다르다면, 길이의 차이 리턴
-      String str = Integer.toString(Integer.parseInt("1212200"));
-      String str2= "2212300";
+      // .equals()    : 참조하는 String 클래스의 값 비교
+      // .compareTo() : 참조하는 String 클래스의 값 비교 (1) 길이 같다면, 다른 char의 차이값 리턴 (2) 길이가 다르다면, 길이의 차이 리턴
+      String str  = Integer.toString(Integer.parseInt("1212200"));
+      String str2 = "2212300";
 
       System.out.println(str.compareTo(str2));
 
-      String revStr = new StringBuilder(str).reverse().toString();
+      String revStr  = new StringBuilder(str).reverse().toString();
       String revStr2 = new StringBuilder(str2).reverse().toString();
 
       System.out.println(revStr2.compareTo(revStr));
         
       // char[] to String
       char[] chArr =  str.toCharArray();
-      String bStr = new String(chArr);
+      String bStr  = new String(chArr);
       String bStr2 = String.valueOf(chArr);
       StringBuilder tmp = new StringBuilder();
 
