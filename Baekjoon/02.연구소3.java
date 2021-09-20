bf527b80bb2e619e7f930e1e91128dfadecb6c03
PoSungKim <psungkr94@likelion.org>
2021-09-21 00:52:25 +0900

    Git Push to Pskim

--- Baekjoon/02.연구소3.java
+++ Baekjoon/02.연구소3.java
@@ -3,7 +3,8 @@ import java.util.*;
 public class Main {
     public static int[][] Dirs = new int[][]{{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
     public static List<Point> List = new ArrayList<>();
-    public static int[][] Board = new int[51][51], Cnt = new int[51][51];
+    public static int[][] Board = new int[51][51];
+    public static int[][] Cnt;
     public static int N, M;
     
     public static class Point { 
@@ -36,54 +37,49 @@ public class Main {
                 int new_x = curPoint.x + Dirs[i][1];
                 if (new_y < 0 || 50 < new_y || new_x < 0 || 50 < new_x)  continue;
                 if (Visited[new_y][new_x] == true) continue;
-                if (Board[new_y][new_x] != 0) continue;
+                if (Board[new_y][new_x] == 1) continue;
                 if (Cnt[new_y][new_x] <= curPoint.turn + 1) continue;
                 
                 Visited[new_y][new_x] = true;
                 Queue.offer(new Point(new_y, new_x, curPoint.turn + 1));
             }
         }
-        System.out.println();
-        System.out.println(point);
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                System.out.print(Cnt[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
+    }
+
+    public static int find_max() {
+        int max_t = 0;
+        for(int i = 0; i < N; i++) 
+            for(int j = 0; j < N; j++) 
+                if (Board[i][j] == 0)
+                    max_t = Math.max(max_t, Cnt[i][j]);
+        
+        return max_t;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt(); M = sc.nextInt();
-        for(int i = 0; i < N; i++) for(int j = 0; j < N; j++) Cnt[i][j] = 100;
-        for(int i = 0; i < N; i++)
+        
+        for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
                 Board[i][j] = sc.nextInt();
-                if (Board[i][j] == 2) {
-                    List.add(new Point(i, j, 1));
-                    Cnt[i][j] = 1;
-                }
-            }
-        
-        for(int i = 0; i < (1 << List.size()); i++) {
-            for(int j = 0; j < List.size(); j++) {
-                for(int k = 0; k < List.size(); k++) 
-                    bfs(List.get(i));
+                if (Board[i][j] == 2) List.add(new Point(i, j, 0));
             }
         }
 
-        
-        System.out.println(List);
-        
-        int max_t = 0;
-        for(int i = 0; i < N; i++) 
-            for(int j = 0; j < N; j++) 
-                if (Cnt[i][j] != 100)
-                    max_t = Math.max(max_t, Cnt[i][j]);
-        
-        System.out.println(max_t);
+        int min_t = 987654321;
+        for(int i = 0; i < (1 << List.size()); i++) {
+            if (Integer.bitCount(i) != M) continue;
+            Cnt = new int[51][51]; for(int k = 0; k < N; k++) for(int l = 0; l < N; l++) Cnt[k][l] = 987654321;
+            for(int j = 0; j < List.size(); j++) {
+                if ( (i & (1 << j)) != 0) {
+                    bfs(List.get(j));
+                }
+            }
+            
+            min_t = Math.min(min_t, find_max());
+        }
+        System.out.println(min_t == 987654321 ? -1 : min_t);
         return;
     }
 }
