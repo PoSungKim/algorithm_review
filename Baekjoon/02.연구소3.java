3596bbd21742ef850d42982579553a7c8d9075cc
PoSungKim <psungkr94@likelion.org>
2021-09-21 13:11:41 +0900

    Git Push to Pskim

--- Baekjoon/02.연구소3.java
+++ Baekjoon/02.연구소3.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class Main {
     public static int[][] Dirs = new int[][]{{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
     public static List<Point> List = new ArrayList<>();
-    public static int[][] Board = new int[51][51];
-    public static int[][] Cnt;
+    public static int[][] Board = new int[50][50];
+    public static final int INF = 987654321;
     public static int N, M;
     
     public static class Point { 
@@ -21,65 +21,63 @@ public class Main {
         }
     }
 
-    public static void bfs(Point point) {
-        Queue<Point> Queue = new LinkedList<>();
-        boolean[][] Visited = new boolean[51][51];
-
-        Queue.offer(point);
-        Visited[point.y][point.x] = true;
-
-        while(!Queue.isEmpty()) {
-            Point curPoint = Queue.poll();
-            Cnt[curPoint.y][curPoint.x] = curPoint.turn;
-            
-            for(int i = 0; i < 4; i++) {
-                int new_y = curPoint.y + Dirs[i][0];
-                int new_x = curPoint.x + Dirs[i][1];
-                if (new_y < 0 || 50 < new_y || new_x < 0 || 50 < new_x)  continue;
-                if (Visited[new_y][new_x] == true) continue;
-                if (Board[new_y][new_x] == 1) continue;
-                if (Cnt[new_y][new_x] <= curPoint.turn + 1) continue;
-                
-                Visited[new_y][new_x] = true;
-                Queue.offer(new Point(new_y, new_x, curPoint.turn + 1));
-            }
-        }
-    }
-
-    public static int find_max() {
-        int max_t = 0;
-        for(int i = 0; i < N; i++) 
-            for(int j = 0; j < N; j++) 
-                if (Board[i][j] == 0)
-                    max_t = Math.max(max_t, Cnt[i][j]);
+    public static int bfs() {
         
-        return max_t;
-    }
+        int min_t = INF;
+        for(int i = 0; i < (1 << List.size()); i++) {
+            if (Integer.bitCount(i) != M) continue;
+            Queue<Point> Queue  = new LinkedList<>();
+            boolean[][] Visited = new boolean[50][50];
+            
+            for(int j = 0; j < List.size(); j++) 
+                if ( (i & (1 << j)) != 0) {
+                    Queue.offer(List.get(j));
+                    Visited[List.get(j).y][List.get(j).x] = true;
+                }
 
+            int max_t = 0;
+            while(!Queue.isEmpty()) {
+                Point curPoint = Queue.poll();
+                if (Board[curPoint.y][curPoint.x] == 0)
+                    max_t = Math.max(max_t, curPoint.turn);
+                
+                for(int dir = 0; dir < 4; dir++) {
+                    int new_y = curPoint.y + Dirs[dir][0];
+                    int new_x = curPoint.x + Dirs[dir][1];        
+
+                    if (new_y < 0 || N <= new_y || new_x < 0 || N <= new_x)  continue;
+                    if (Visited[new_y][new_x] || Board[new_y][new_x] == 1)     continue;
+                    
+                    Visited[new_y][new_x] = true;
+                    Queue.offer(new Point(new_y, new_x, curPoint.turn + 1));
+                }
+            }
+
+            boolean flag = true;
+            for(int r = 0; r < N; r++) 
+                for(int c = 0; c < N; c++) 
+                    if (Board[r][c] == 0 && !Visited[r][c]) {
+                        flag = false;
+                        break;
+                    }    
+            
+            if (flag)
+                min_t = Math.min(min_t, max_t);
+        }
+
+        return min_t == INF ? -1 : min_t;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt(); M = sc.nextInt();
         
-        for(int i = 0; i < N; i++) {
+        for(int i = 0; i < N; i++) 
             for(int j = 0; j < N; j++) {
                 Board[i][j] = sc.nextInt();
                 if (Board[i][j] == 2) List.add(new Point(i, j, 0));
             }
-        }
 
-        int min_t = 987654321;
-        for(int i = 0; i < (1 << List.size()); i++) {
-            if (Integer.bitCount(i) != M) continue;
-            Cnt = new int[51][51]; for(int k = 0; k < N; k++) for(int l = 0; l < N; l++) Cnt[k][l] = 987654321;
-            for(int j = 0; j < List.size(); j++) {
-                if ( (i & (1 << j)) != 0) {
-                    bfs(List.get(j));
-                }
-            }
-            
-            min_t = Math.min(min_t, find_max());
-        }
-        System.out.println(min_t == 987654321 ? -1 : min_t);
+        System.out.println(bfs());
         return;
     }
 }
