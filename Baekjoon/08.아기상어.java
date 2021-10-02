3bae49449d2e0a2db59afe8e9f682c609d589626
PoSungKim <psungkr94@likelion.org>
2021-10-02 22:54:48 +0900

    Git Push to Pskim

--- Baekjoon/08.아기상어.java
+++ Baekjoon/08.아기상어.java
@@ -26,7 +26,7 @@ public class Main {
     public static int solve() {
 
         while(true) {
-            for(int i = 0 ; i < N; i ++) for(int j = 0; j < N; j++) Visited[i][j] = 0;
+            for(int i = 0 ; i < N; i ++) for(int j = 0; j < N; j++) Visited[i][j] = false;
             List.clear();
             Queue.clear();
 
