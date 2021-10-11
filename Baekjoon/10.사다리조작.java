5ee173511f2503172e3b0c111fb835bb058f752a
PoSungKim <psungkr94@likelion.org>
2021-10-11 18:00:39 +0900

    Git Push to Pskim

--- Baekjoon/10.사다리조작.java
+++ Baekjoon/10.사다리조작.java
@@ -7,11 +7,11 @@ public class Main {
     public static final int INF = 987654321;
     public static int N, M, H, ans = INF;
     
-    public static boolean solve() {
-        for(int c = 1; c <= N; c++) {
+    public static boolean is_okay() {
+        for(int c = 0; c < N; c++) {
             int cur_pos = c;
-            for(int r = 1; r <= H; r++) 
-                if (cur_pos < N && Board[r][cur_pos][1] && Board[r][cur_pos + 1][0]) 
+            for(int r = 0; r < H; r++) 
+                if (cur_pos < N - 1 && Board[r][cur_pos][1] && Board[r][cur_pos + 1][0]) 
                     cur_pos++;
                 else if (0 < cur_pos && Board[r][cur_pos - 1][1] && Board[r][cur_pos][0])
                     cur_pos--;
@@ -21,20 +21,41 @@ public class Main {
         return true;
     }
     
-    public static void comb(int y, int x, int cnt) {
-        if (cnt > 3 || y * 10 + x == N * H) return;
-        if (solve()) ans =  Math.min(ans, cnt);
+    public static void comb2(int y, int x, int cnt) {
+        if (cnt > 3 || y * N + x == N * H) return;
+        if (is_okay()) ans =  Math.min(ans, cnt);
         
         for(int i = 1; i <= H; i++) {
             for(int j = 1; j <= N; j++) {
-                if (y * 10 + x > i * 10 + j) continue;
+                if (y * N + x > i * N + j) continue;
                 if (j < N && !Board[i][j][1] && !Board[i][j + 1][0]) {
                     Board[i][j][1] = Board[i][j + 1][0] = true;
-                    comb(i, j, cnt + 1);
+                    comb2(i, j, cnt + 1);
                     Board[i][j][1] = Board[i][j + 1][0] = false;
                 }
             }
         }
+    }
+
+    public static int comb(int pos, int cnt) {
+        //왼 : 3개를 골랐을 때
+        //오 : 3개 이하를 골랐을 때
+        if (cnt == 3 || pos >= N * H) {
+            if (is_okay()) return cnt;
+            return INF;
+        }
+        
+        int ret = INF;
+        int y = pos / N;
+        int x = pos % N;
+        if (x < N - 1 && !Board[y][x][1] && !Board[y][x + 1][0]) {
+            Board[y][x][1] = Board[y][x + 1][0] = true;
+            ret = Math.min(ret, comb(pos + 2, cnt + 1));
+            Board[y][x][1] = Board[y][x + 1][0] = false;
+        }
+        ret = Math.min(ret, comb(pos + 1, cnt));
+
+        return ret;
     }
 
     public static void main(String[] args) throws IOException{
@@ -44,9 +65,13 @@ public class Main {
         for(int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
-            Board[a][b][1] = Board[a][b + 1][0] = true;
+            //첫 풀이) Board[a][b][1] = Board[a][b + 1][0] = true;
+            Board[a - 1][b - 1][1] = Board[a - 1][b][0] = true;
         }
-        comb(0, 0, 0);
-        System.out.println(ans > 3? -1 : ans);
+        //첫 풀이) comb2(1, 1, 0);
+        //첫 풀이) System.out.println(ans > 3? -1 : ans);
+        
+        int ret = comb(0, 0);
+        System.out.println(ret == INF ? -1 : ret);
     }
 }
