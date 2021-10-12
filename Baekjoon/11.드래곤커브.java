1fc276e599beb0bd2cd7f897947660e143046d50
PoSungKim <psungkr94@likelion.org>
2021-10-12 15:09:59 +0900

    Git Push to Pskim

--- Baekjoon/11.드래곤커브.java
+++ Baekjoon/11.드래곤커브.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Main {
-    public static int[][] Dirs = new int[][] {{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
+    public static int[][] Dirs = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
     public static boolean[][] Board = new boolean [101][101];
     public static List<Integer> List = new ArrayList<>();
     public static int N;
@@ -25,16 +25,16 @@ public class Main {
             int y = sc.nextInt();
             int d = sc.nextInt();
             int g = sc.nextInt();
+            
             List.add(d);
-            for(int j = 0; j < g; j++) {
-                for(int k = List.size() - 1; k >= 0; k--) {
+            for(int j = 0; j < g; j++) 
+                for(int k = List.size() - 1; k >= 0; k--) 
                     List.add((List.get(k) + 1) % 4);
-                }
-            }
+            
             Board[y][x] = true;
-            for(int j = 0; j < List.size(); j++) {
-                y = y + Dirs[List.get(j)][0];
-                x = x + Dirs[List.get(j)][1];
+            for(int Dir : List) {
+                y += Dirs[Dir][0];
+                x += Dirs[Dir][1];
                 Board[y][x] = true;
             }
         }
