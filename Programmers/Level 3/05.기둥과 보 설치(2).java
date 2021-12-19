1dcfdc62921960f35e9afd671cd4ef0c49cf7eb8
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-12-19 23:25:46 +0900

    Update 05.기둥과 보 설치(2).java

--- Programmers/Level 3/05.기둥과 보 설치(2).java
+++ Programmers/Level 3/05.기둥과 보 설치(2).java
@@ -3,56 +3,72 @@ import java.util.*;
 class Solution {
     
     public boolean[][][] board;
-    public int N;
     
-    public boolean check(int x, int y, int a){
-        
-        if (a == 0) { // 기둥
-            if (y == 0) { // 바닥이거나
+    public boolean isOkay(int x, int y, int a) {
+        if (a == 0) {
+            if (y == 0) {
                 return true;
-            } else if (0 <= x - 1 && board[y][x - 1][1] || board[y][x][1]) { // 보의 한쪽 끝
+            } else if (x - 1 >= 0 && board[y][x - 1][1] || board[y][x][1]) {
                 return true;
-            } else if (0 <= y - 1 && board[y - 1][x][0]) { // 또 다른 기둥 위
+            } else if (y - 1 >= 0 && board[y - 1][x][a]) {
                 return true;
-            }
-        } else { // 보
-            if ( (0 <= y - 1 && board[y - 1][x][0]) || (0 <= y - 1 && x + 1 < N && board[y - 1][x + 1][0]) ) { // 한쪽 끝 부분이 기둥 위
+            } 
+        } else {
+            if (x - 1 >= 0 && board[y][x - 1][a] && x + 1 < board.length && board[y][x + 1][a]) {
                 return true;
-            } else if (0 <= x - 1 && board[y][x - 1][1] && x + 1 < N && board[y][x + 1][1]) { // 양쪽 끝 부분이 다른 보와 동시에 연결
+            } else if ( (y - 1 >= 0 && board[y - 1][x][0]) || (y - 1 >= 0 && x + 1 < board.length && board[y - 1][x + 1][0]) ) {
                 return true;
             }
         }
-        
         return false;
     }
     
-    public void add(int x, int y, int a){
-        if (check(x, y, a))
+    public void build(int x, int y, int a) {
+        if (isOkay(x, y, a))
             board[y][x][a] = true;
     }
     
-    public void remove(int x, int y, int a){
-        
+    public void delete(int x, int y, int a) {
         board[y][x][a] = false;
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                for(int k = 0; k < 2; k++) {
-                    if (board[i][j][k]) {
-                        if (!check(j, i, k)){
+        for(int i = 0; i < board.length; i++) 
+            for(int j = 0; j < board[i].length; j++) 
+                for(int k = 0; k < 2; k++) 
+                    if (board[i][j][k]) 
+                        if (!isOkay(j, i, k)) 
                             board[y][x][a] = true;
-                            return;
-                        }
-                    }    
-                }
-            }
-        }
+            
+    }
+    
+    public int[][] emitAns() {
+        List<int[]> ansList = new ArrayList<>();
+        for(int i = 0; i < board.length; i++) 
+            for(int j = 0; j < board[i].length; j++) 
+                for(int k = 0; k < 2; k++) 
+                    if (board[i][j][k]) 
+                        ansList.add(new int[] {j, i, k});
         
+        int[][] ans = new int[ansList.size()][3];
+        for(int i = 0; i < ansList.size(); i++) 
+            for(int j = 0; j < 3; j++) 
+                ans[i][j] = ansList.get(i)[j];
+        
+        Arrays.sort(ans, (a,b)->{
+            
+            if(a[0] == b[0]) {
+                if (a[1] == b[2])
+                    return a[2] - b[2];
+                return a[1] - b[1];
+            }
+            
+            return a[0] - b[0];
+        });
+        
+        return ans;
     }
     
     public int[][] solution(int n, int[][] build_frame) {
-        n++;
-        board = new boolean[n][n][2];
-        N = n;
+        
+        board = new boolean[n + 1][n + 1][2];
         
         for(int[] frame : build_frame) {
             int x = frame[0],
@@ -60,45 +76,11 @@ class Solution {
                 a = frame[2],
                 b = frame[3];
             
-            if (b == 1) {
-                add(x, y, a);
-            } else {
-                remove(x, y, a);
-            }
-        }
-        
-        List<int[]> ansList = new ArrayList<>();
-        
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                for(int k = 0; k < 2; k++) {
-                    if (board[i][j][k]) {
-                        ansList.add(new int[]{j, i, k});
-                    }
-                }
-            }
-        }
-        
-        Collections.sort(ansList, (a, b)-> {
-            if (a[0] == b[0]) {
-                if (a[1] == b[1]) 
-                    return a[2] - b[2];
-                return a[1] - b[1];
-            }
-            return a[0] - b[0];
-        });
-        
-        if (ansList.size() == 0)
-            return new int[][]{};
-        
-        int[][] answer = new int[ansList.size()][3];
-        for(int i = 0; i < ansList.size(); i++) {
-            int[] a = ansList.get(i);
-            answer[i][0] = a[0];
-            answer[i][1] = a[1];
-            answer[i][2] = a[2];
-        }
+            if (b == 1) build(x, y, a);    
+            else delete(x, y, a);
             
-        return answer;
+        }
+        
+        return emitAns();
     }
 }
