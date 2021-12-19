dca07348600741145ea2cecaf9373fc2ad2ed474
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2021-12-19 17:43:30 +0900

    Update 13.N-Queen.java

--- Programmers/Level 3/13.N-Queen.java
+++ Programmers/Level 3/13.N-Queen.java
@@ -5,21 +5,25 @@ class Solution {
     public int board[];
     public int answer = 0;
     
+    public boolean isOkay(int pos) {
+        for(int j = 0; j < pos; j++) {
+            if (board[j] == board[pos]) 
+                return false ;
+            if (Math.abs(j - pos) == Math.abs(board[j] - board[pos])) 
+                return false ;
+        }
+        return true;
+    }
+    
     public void perm(int pos) {
         
         if (pos == board.length) {
             answer++;
             return;
         }
-        
         for(int i = 1; i <= board.length; i++) {
             board[pos] = i;
-            boolean isOkay = true;
-            for(int j = 0; j < pos; j++) {
-                if (board[j] == board[pos]) isOkay = false;
-                if (Math.abs(j - pos) == Math.abs(board[j] - board[pos])) isOkay = false;
-            }
-            if (isOkay)
+            if (isOkay(pos)) 
                 perm(pos + 1);
         }
     }
