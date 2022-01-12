46e09524075c1db3b25a142b8ff2591ffc95bedd
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2022-01-12 13:49:15 +0900

    Update 11-3.Pattern,Compile.java

--- Java/11-3.Pattern,Compile.java
+++ Java/11-3.Pattern,Compile.java
@@ -3,7 +3,8 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class MyClass {
-    public static void main(String args[]) {
+    
+    public static void countWords(String args[]) {
       Pattern pattern = Pattern.compile("\\bhello\\b");
       Matcher matcher = pattern.matcher("hello hello hello hello hello world!!");
       
@@ -11,6 +12,17 @@ public class MyClass {
       while(matcher.find()) {
           System.out.printf("count : %s\n", ++cnt);
           System.out.printf("group : %s\n", matcher.group());
+          System.out.printf("start : %s\n", matcher.start());
+          System.out.printf("end   : %s\n\n", matcher.end());
+      }
+    }
+    
+    public static void groupSecond(String args[]) {
+      Pattern pattern = Pattern.compile("(hello) (hellow)");
+      Matcher matcher = pattern.matcher("hello hellow hello hellow hello world!!");
+      
+      while(matcher.find()) {
+          System.out.printf("group : %s\n", matcher.group(2)); //hellow x 2
           System.out.printf("start : %s\n", matcher.start());
           System.out.printf("end   : %s\n\n", matcher.end());
       }
