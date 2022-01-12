c31132813e634e76258e35b8443185326efff444
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2022-01-12 13:39:31 +0900

    Update 11-3.Pattern,Compile.java

--- Java/11-3.Pattern,Compile.java
+++ Java/11-3.Pattern,Compile.java
@@ -1,24 +1,18 @@
 // https://codechacha.com/ko/java-regex/
-import java.util.Pattern;
-import java.util.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
-class Solution {
-    // Pattern 
-  
-    public int Pattern() {
-        String regex = "\\bHello\\b";
-        Pattern pattern = Pattern.compile(regex);
-      
-        String target = "Hello Hello Hello Hello World";
-        Matcher matcher = pattern.matcher(target);
-      
-        int cnt = 0;
-        while(matcher.find()) {
-            cnt++;
-            System.out.printf("Count     : %s \n", cnt);
-            System.out.printf("Group     : %s \n", matcher.group());
-            System.out.printf("Start Idx : %s \n", matcher.start());
-            System.out.printf("End Idx   : %s \n", matcher.end());
-        }  
+public class MyClass {
+    public static void main(String args[]) {
+      Pattern pattern = Pattern.compile("\\bhello\\b");
+      Matcher matcher = pattern.matcher("hello hello hello hello hello world!!");
+      int cnt = 0;
+
+      while(matcher.find()) {
+          System.out.printf("count : %s\n", ++cnt);
+          System.out.printf("group : %s\n", matcher.group());
+          System.out.printf("start : %s\n", matcher.start());
+          System.out.printf("end   : %s\n\n", matcher.end());
+      }
     }
 }
