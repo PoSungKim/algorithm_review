5c660251081c2946f004de28cf87058ecb77a015
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2022-01-14 00:22:14 +0900

    Update 17.110옮기기.java
    Personally, this problem is one of memorable problems because a concept of Stack's LIFO is still confusing me with another concept of `sb.insert(0, char c)`

--- Programmers/Level 3/17.110옮기기.java
+++ Programmers/Level 3/17.110옮기기.java
@@ -1,1 +1,63 @@
+import java.util.*;
 
+class Solution {
+    
+    public String getMinDict(String str) {
+        
+        Stack<Character> Stack = new Stack<>();
+        int cnt = 0;
+        for(char c : str.toCharArray()){
+            
+            if (Stack.size() >= 2) {
+                char x = Stack.pop();
+                char y = Stack.pop();
+                
+                if (x == '1' && y == '1' && c =='0') {
+                    cnt++;
+                    continue;
+                } else {
+                    Stack.push(y);
+                    Stack.push(x);
+                }
+            }
+            
+            Stack.push(c);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        boolean isZero = false;
+        int idx = Stack.size();
+        while(!Stack.isEmpty()) {
+            char c = Stack.pop();
+            
+            if (c == '0') {
+                isZero = true;
+            }
+            
+            if (!isZero && c == '1') {
+                idx--;
+            }
+            
+            sb.insert(0, c);
+        }
+        
+        if (cnt > 0) {
+            while(cnt-- > 0) {
+                sb.insert(idx, "110");
+                idx += 3;
+            }
+        } else return sb.toString();
+        
+        return sb.toString();
+    }
+    
+    public String[] solution(String[] s) {
+        String[] answer = new String[s.length];
+        
+        for(int i = 0; i < s.length; i++) 
+            answer[i] = getMinDict(s[i]);
+        
+        return answer;
+    }
+    
+}
