d8890be8887b530ec845dad04f596405e889ef4e
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2022-03-09 16:13:49 +0900

    Update 3.Longest Substring Without Repeating Characters.java

--- LeetCode/3.Longest Substring Without Repeating Characters.java
+++ LeetCode/3.Longest Substring Without Repeating Characters.java
@@ -2,22 +2,22 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         
         char[] charArray = s.toCharArray();
-        int[] numArray = new int[s.length()];
+        LinkedList<Character> charQueue = new LinkedList<>();
+        int ans = 0;
         
-        for(int i = 0; i < charArray.length; i++) {
-            Set<Character> strSet = new HashSet<>();
-            strSet.add(charArray[i]);
-            numArray[i] = 1;
-            for(int j = i + 1; j < charArray.length; j++) {
-                if (!strSet.contains(charArray[j])) {
-                    strSet.add(charArray[j]);
-                    numArray[i]++;
-                } else break;
+        for(char ch : charArray) {
+            if (!charQueue.contains(ch)) {
+                charQueue.offer(ch);
+                ans = Math.max(ans, charQueue.size());
+            } else {
+                
+                while (charQueue.size() > 0 && charQueue.peek().charValue() != ch)
+                    charQueue.poll();
+                
+                if (charQueue.size() > 0 && charQueue.peek().charValue() == ch)
+                    charQueue.offer(charQueue.poll());
             }
         }
-        
-        int ans = 0;
-        for(int num : numArray) ans = Math.max(ans, num);
         
         return ans;
     }
