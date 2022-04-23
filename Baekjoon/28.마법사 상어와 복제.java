b5916ddad4f151ad463fefd7f47999ff82ec0846
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2022-04-23 21:20:48 +0900

    Update 28.마법사 상어와 복제.java

--- Baekjoon/28.마법사 상어와 복제.java
+++ Baekjoon/28.마법사 상어와 복제.java
@@ -1,2 +1,2 @@
 // https://velog.io/@mincho920/%EB%B0%B1%EC%A4%8023290Java-%EB%A7%88%EB%B2%95%EC%82%AC-%EC%83%81%EC%96%B4%EC%99%80-%EB%B3%B5%EC%A0%9C%EC%95%88%EA%B0%84%EB%8B%A8-%ED%92%80%EC%9D%B4
-
+// 주요 연산 : nd = (this.d - i + 8) % 8; 반대 45도로 진행될 때;
