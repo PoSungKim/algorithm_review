9e99b74d6d141725ece6b982e6cfb7a651a03d6d
Po Sung Kim <37537227+PoSungKim@users.noreply.github.com>
2023-02-26 18:44:33 +0900

    Create 0034.(BinarySearch)Find First and Last Position of Element in Sorted Array.java

---
+++ LeetCode/0034.(BinarySearch)Find First and Last Position of Element in Sorted Array.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public int binarySearch(int[] nums, int target, int left, int right) {
+
+        while(left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] < target) 
+                left = mid + 1;
+            else // 즉, nums[mid] == target일 때도 mid - 1을 right에 넣어줌으로써 lower bound 값을 연산하게 된다는 의미
+                right = mid - 1;
+        }
+
+        return left;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+
+        int left = binarySearch(nums, target, 0, nums.length - 1);
+        int right = binarySearch(nums, target + 1, 0, nums.length - 1) - 1;
+
+        if (left > 0 && nums[left] == target)
+            return new int[] {left, right};
+
+        return new int[] {-1, -1};
+    }
+}
